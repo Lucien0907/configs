423d9316a9064ba2d8d265a893c9e04121c125e6
Tao-Wei Chan <taowei.c@outlook.com>
2025-03-03 22:48:52 +0000

    update

--- custom_setup.sh
+++ custom_setup.sh
@@ -45,23 +45,28 @@ echo $PROTOCOL
 echo $EMAIL
 
 # For ghostty
+echo '############################## setting TERM for ghostty ##############################'
 if ! grep -q "1i export TERM=xterm-256color" ~/.bashrc; then
   sed -i '1i export TERM=xterm-256color' ~/.bashrc
   source ~/.bashrc
 fi
 
+echo '############################## updrade apt packages ##############################'
 sudo apt update && sudo apt upgrade -y
 
 # Generate ssh key pair
+echo '############################## generate ssh key pair ##############################'
 ssh-keygen -t rsa -C tao-wei_chan -f ~/.ssh/id_rsa -N ""
 cat ~/.ssh/id_rsa.pub
 read -p "Copy and add the public key to github and ado, then Enter to continue..."
 
 # Set up git
+echo '############################## set up git user name & email ##############################'
 git config --global user.name "Tao-Wei Chan"
 git config --global user.email $EMAIL
 
 # Set up tmux
+echo '############################## set up tmux ##############################'
 mkdir -p ~/dev
 git clone $GITHUB_BASE_URL/configs.git ~/dev/configs
 cp ~/dev/configs/.tmux.conf ~/
@@ -71,6 +76,8 @@ git clone -b v2.1.2 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugin
 ~/.tmux/plugins/tpm/bin/install_plugins
 
 # Install yazi
+echo '############################## install yazi ##############################'
+sudo apt update && sudo apt install -y unzip
 if ! grep -q 'export PATH="/opt/yazi:$PATH"' ~/.bashrc; then
   echo 'export PATH="/opt/yazi:$PATH"' >>~/.bashrc
   source ~/.bashrc
@@ -89,7 +96,6 @@ if ! grep -q "function y() {" ~/.bashrc; then
   echo "$YAZI_FUNC" >>~/.bashrc
 fi
 sudo rm -rf /opt/yazi
-sudo apt update && sudo apt install -y unzip
 git clone $GITHUB_BASE_URL/yazi-config.git ~/.config/yazi
 curl -L -o yazi.zip https://github.com/sxyazi/yazi/releases/download/v25.3.2/yazi-x86_64-unknown-linux-gnu.zip
 unzip -q yazi.zip
@@ -97,6 +103,12 @@ sudo mv yazi-x86_64-unknown-linux-gnu /opt/yazi
 rm yazi.zip
 
 # Install pyenv
+echo '############################## install pyenv ##############################'
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y build-essential libssl-dev zlib1g-dev \
+  libbz2-dev libreadline-dev libsqlite3-dev curl git \
+  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
 for line in \
   'export PYENV_ROOT="$HOME/.pyenv"' \
   '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' \
@@ -106,28 +118,29 @@ for line in \
 done
 source ~/.bashrc
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y build-essential libssl-dev zlib1g-dev \
-  libbz2-dev libreadline-dev libsqlite3-dev curl git \
-  libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
-curl -fsSL https://pyenv.run | bash -s -- -y
+curl -fsSL https://pyenv.run | bash
 pyenv install 3.10.16
 pyenv global 3.10.16
 pyenv versions
 
-# Install pipx and poetry
+# Install pipx
+echo '############################## install pipx ##############################'
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y pipx
 pipx ensurepath
+
+# Install poetry
+echo '############################## install poetry ##############################'
 pipx install poetry
 pipx inject poetry poetry-plugin-shell
 source ~/.bashrc
 
 # Install neovim
+echo '############################## install neovim ##############################'
 if ! grep -q 'export PATH="/opt/nvim-linux-x86_64/bin:$PATH"' ~/.bashrc; then
   echo 'export PATH="/opt/nvim-linux-x86_64/bin:$PATH"' >>~/.bashrc
 fi
+sudo apt update && sudo apt install -y fd-find rigrep
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
@@ -137,15 +150,35 @@ git clone -b ubuntu $GITHUB_BASE_URL/nvim.git ~/.config/nvim
 sudo apt install -y luarocks fzf
 rm -f nvim-linux-x86_64.tar.gz
 
+echo '############################## set up pynvim venv ##############################'
+NVIM_OPTION_FILE="$HOME/.config/nvim/lua/config/options.lua"
+pyenv virtualenv 3.10.16 pynvim
+PYENV_VERSION=pynvim pyenv exec pip install --upgrade pip
+PYENV_VERSION=pynvim pyenv exec pip install pynvim
+if [ -f $NVIM_OPTION_FILE ]; then
+  echo "Update neovim python provider in $NVIM_OPTION_FILE "
+  if grep -q 'vim.g.python3_host_prog =' "$NVIM_OPTION_FILE"; then
+    sed -i "s|vim.g.python3_host_prog = \".*\"|vim.g.python3_host_prog = \"$(pyenv prefix pynvim)/bin/python\"|" ~/.config/nvim/lua/config/options.lua
+  else
+    echo "" >>"$NVIM_OPTION_FILE"
+    echo "vim.g.python3_host_prog = \"$(pyenv prefix pynvim)/bin/python\"" >>"$NVIM_OPTION_FILE"
+  fi
+else
+  echo "File $NVIM_OPTION_FILE does not exist"
+fi
+
 # Download and install nvm
+echo '############################## install nvm ##############################'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 nvm install 22
 node -v
 nvm current
 npm -v
+npm install -g neovim
 
 # install lazygit
+echo '############################## install lazygit ##############################'
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
@@ -155,10 +188,12 @@ rm -f lazygit.tar.gz
 rm -rf lazygit
 
 # install lazydocker
+echo '############################## install lazydocker ##############################'
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 lazydocker --version
 
 # install docker
+echo '############################## install docker ##############################'
 # Add Docker's official GPG key:
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl
@@ -169,7 +204,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
+   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" |
   sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt-get update
 
